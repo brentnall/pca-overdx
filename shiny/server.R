e9159761ce33b7e9b5912b3683e85dae12d9f826
brentnall <a.brentnall@qmul.ac.uk>
2025-12-12 22:58:06 +0000

    bug fix with updating stats/figures

--- shiny/server.R
+++ shiny/server.R
@@ -1,9 +1,6 @@
 # Define server logic to plot various variables against mpg ----
 server <- function(input, output) {
 
-
-
-   
     output$plot1 <- renderPlot({
         validate(
 		need(input$inage >=40 & input$inage <=85, "Error - Age must be between 40y and 85y"),
@@ -12,9 +9,7 @@ server <- function(input, output) {
         palette(c("#E41A1C", "#377EB8", "#4DAF4A", "#984EA3",
                   "#FF7F00", "#FFFF33", "#A65628", "#F781BF", "#999999"))
 
-        myadj<-input$inadj
-
-    myout<<-sapply(50:85, function(inage) fn.overdxc2(inage, myadj))
+    myout<<-sapply(50:85, function(inage) fn.overdxc2(inage, input$inadj))
 
     plot(50:85,myout[1,]*100, type="l", ylim=c(0,100), ylab="Probability die in 15y", xlab="Age",lwd=2)
     grid()
@@ -25,7 +20,9 @@ server <- function(input, output) {
 
     output$plot2 <- renderPlot({
 
-        myadj<-input$inadj     
+        validate(
+            need(input$inage >=40 & input$inage <=85, "Error - Age must be between 40y and 85y"),
+            need(input$inadj>0, "Error - Adjustment for life expectancy must be >0"))
 
         plot(50:85,myout[2,]*100, type="l", ylim=c(0,100), ylab="Probability overdiagnosis, if diagnosed now", xlab="Age", lwd=2, col=4)
         grid()
@@ -38,9 +35,9 @@ server <- function(input, output) {
 	validate(
 		need(input$inage >=40 & input$inage <=85, "Error - Age must be between 40y and 85y"),
 		need(input$inadj>0, "Error - Adjustment for life expectancy must be >0"))
-
-        ##        paste0("Probability die in 15y, from given age: ", paste0(fn.format(100*myout[1,input$inage-49],1),"%"))
-        paste0("For every 1000 men aged ", input$inage, ", ", fn.format(1000*myout[1,input$inage-49],0), " are expected to die in the next 15y")
+        
+        paste0("Probability die in 15y from age ", input$inage, ": ", paste0(fn.format(100*myout[1,input$inage-49],1),"%"))
+##        paste0("For every 1000 men aged ", input$inage, ", ", fn.format(1000*myout[1,input$inage-49],0), " are expected to die in the next 15y")
         
     })
     
